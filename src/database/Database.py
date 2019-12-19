693b1881b4cc450831c84e9564030dd0f9a461d6
anoy <kuma.tola3@gmail.com>
2019-12-19 10:15:34 +0900

    fix path to database

--- src/database/Database.py
+++ src/database/Database.py
@@ -6,24 +6,27 @@ from src.definition import ROOT
 class Database:
     def __init__(self, table_name):
         self.table_name = table_name
+        self.path = f'{ROOT}/database/SQlite3'
+
+    def _connect_db(self):
+        conn = sqlite3.connect(self.path)
+        c = conn.cursor()
+        return conn, c
 
     def save(self, data):
-        conn = sqlite3.connect(f'{ROOT}/src/database/SQlite3')
-        c = conn.cursor()
+        conn, c = self._connect_db()
         c.execute(f'INSERT INTO {self.table_name} VALUES(?);', (data,))
         conn.commit()
         conn.close()
 
     def check(self, data):
-        conn = sqlite3.connect(f'{ROOT}/src/database/SQlite3')
-        c = conn.cursor()
+        conn, c = self._connect_db()
         res = c.execute(f'SELECT * FROM {self.table_name} WHERE data=?;', (data,)).fetchone()
         conn.close()
         return res is not None
 
     def check_ids(self, data):
-        conn = sqlite3.connect(f'{ROOT}/src/database/SQlite3')
-        c = conn.cursor()
+        conn, c = self._connect_db()
 
         results = []
         for d in data:
@@ -34,8 +37,7 @@ class Database:
         return results
 
     def delete(self):
-        conn = sqlite3.connect(f'{ROOT}/src/database/SQlite3')
-        c = conn.cursor()
+        conn, c = self._connect_db()
         c.execute(f'DELETE FROM {self.table_name};')
         conn.commit()
         conn.close()
